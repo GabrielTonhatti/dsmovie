a71ff7f7631517b1e5ffedd92b2e31ec06755568
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-01-11 22:49:43 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,29 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
 
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 
 }
